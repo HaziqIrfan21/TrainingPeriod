bdbbffd9715957c30d99a6e0b53f23b2a90a384c
HaziqIrfan21 <90903576+HaziqIrfan21@users.noreply.github.com>
2021-09-20 07:14:44 +0800

    Add files via upload
    -Push / pop / Clear Stack

---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+
+namespace StackQuestion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Create new stack
+            Stack myStack = new Stack();
+
+            //PUSH new elements into the stack
+            myStack.Push("book 1");
+            myStack.Push("book 2");
+            myStack.Push("book 3");
+            myStack.Push("book 4");
+            myStack.Push("book 5");
+
+            //Display element count
+            Console.Write(" Amount of books are: ");
+
+            Console.WriteLine(myStack.Count);
+
+            //POP out books
+            Console.Write(" Pop Out ");
+            Console.Write(myStack.Pop());
+
+            //PEEK at next book
+            Console.WriteLine(" Peek at next book to pop {0} ", myStack.Peek());
+
+            //Display new element count after POP
+            Console.Write(" amount of book ");
+            Console.WriteLine(myStack.Count);
+
+            //POP out books
+            Console.Write(" Pop Out ");
+            Console.Write(myStack.Pop());
+
+            //PEEK at next book
+            Console.WriteLine(" Peek at next book to pop {0}", myStack.Peek());
+
+            //Display new element count after POP
+            Console.Write(" amount of book ");
+            Console.WriteLine(myStack.Count);
+
+            //CLEAR stack
+            Console.Write(" Clearing the stack ");
+            myStack.Clear();
+
+            //Display new element count after CLEAR
+            Console.Write(" amount of book ");
+            Console.WriteLine(myStack.Count);
+
+
+        }
+    }
+}
