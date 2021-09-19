9f4576ee07acad45b16eadc4b4e9d5c2594fc828
HaziqIrfan21 <90903576+HaziqIrfan21@users.noreply.github.com>
2021-09-20 07:09:52 +0800

    Add files via upload
    -Using Getter and setter
    - Using nested struct

---
+++ ConsoleApp2/ConsoleApp2/Employee.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Employee
+    {
+        //private string name;
+        //private int yearOfJoining;
+        //private int salary;
+
+        struct employee
+        {
+            public string name;
+            public record records;
+        }
+
+        struct record
+        {
+            public int salary;
+            public int YearOfJoining;
+
+        }
+
+        //public string Name
+        //{
+        //    get => name;
+        //    set => name = value;
+        //}
+
+        //public int YearOfJoining
+        //{
+        //    get => yearOfJoining;
+        //    set => yearOfJoining = value;
+        //}
+
+        //public int Salary
+        //{
+        //    get => salary;
+        //    set => salary = value;
+        //}
+
+        static void Main(string[] args)
+        {
+            int totalEmployees;
+            Console.WriteLine("amount of employees");
+            totalEmployees = Convert.ToInt32(Console.ReadLine());
+
+
+            employee[] employees = new employee[totalEmployees];
+
+            for (int i = 0; i < totalEmployees; i++)
+            {
+
+                //Name of employees
+                Console.WriteLine("Name of employees");
+                string name = Console.ReadLine();
+                employees[i].name = name;
+
+                //Salary of employees
+
+                Console.WriteLine("Salary of employees");
+                int salary = Convert.ToInt32(Console.ReadLine());
+                employees[i].records.salary = salary;
+
+                //Year of employees
+
+                Console.WriteLine("Year Joined");
+                int Joined = Convert.ToInt32(Console.ReadLine());
+                employees[i].records.YearOfJoining = Joined;
+
+                Console.WriteLine("Full employee record " + name +" " + salary + " " +Joined);
+            }
+
+
+
+        }
+
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        //Console.WriteLine("Hello World!");
+
+    //        studentClass sam = new studentClass();
+
+    //        sam.RollId = 1;
+    //        sam.StudentName = "Sam";
+    //        sam.PhoneNumber = 12341234;
+    //        sam.Address = "BLK Example 1";
+
+    //        studentClass john = new studentClass();
+
+    //        john.RollId = 2;
+    //        john.StudentName = "John";
+    //        john.PhoneNumber = 56785678;
+    //        john.Address = "BLK Example 2";
+
+    //        Console.WriteLine(sam.RollId);
+    //        Console.WriteLine(sam.StudentName);
+    //        Console.WriteLine(sam.PhoneNumber);
+    //        Console.WriteLine( sam.Address);
+
+    //        Console.WriteLine("");
+
+    //        Console.WriteLine(john.RollId);
+    //        Console.WriteLine(john.StudentName);
+    //        Console.WriteLine(john.PhoneNumber);
+    //        Console.WriteLine(john.Address);
+
+    //        Console.WriteLine("");
+    //        Console.WriteLine("");
+
+    //        Employee Alpha = new Employee();
+    //        Alpha.Name = "Alpha";
+    //        Alpha.YearOfJoining = 2000;
+    //        Alpha.Salary = 3100;
+
+    //        Employee Bravo = new Employee();
+    //        Bravo.Name = "Bravo";
+    //        Bravo.YearOfJoining = 2001;
+    //        Bravo.Salary = 3150;
+
+    //        Employee Charlie = new Employee();
+    //        Charlie.Name = "Charlie";
+    //        Charlie.YearOfJoining = 2002;
+    //        Bravo.Salary = 3200;
+
+    //        Console.Write(Alpha.Name+ " " + Alpha.YearOfJoining+ " "+ Alpha.Salary);
+    //        Console.Write(Bravo.Name + " " + Bravo.YearOfJoining + " " + Bravo.Salary);
+    //        Console.Write(Charlie.Name + " " + Charlie.YearOfJoining + " " + Charlie.Salary);
+
+
+    //        //int[] array = { 1,2,3,4,5};
+
+    //        // int size;
+    //        // int amount;
+    //        // int result;
+
+    //        //int totalEmployees;
+    //        //Console.WriteLine("amount of employees");
+    //        //totalEmployees = Convert.ToInt32(Console.ReadLine());
+           
+
+    //        //Employee[] employees = new Employee[totalEmployees];
+
+    //        //for (int i = 0; i < totalEmployees; i++)
+    //        //{
+
+    //        //    //Name of employees
+    //        //    Console.WriteLine("Name of employees");
+    //        //    string name = Console.ReadLine();
+    //        //    employees[i].Name = name;
+
+    //        //    //ID of employees
+
+    //        //    Console.WriteLine("ID of employees");
+    //        //    int id = Convert.ToInt32(Console.ReadLine());
+    //        //    employees[i].ID
+
+
+
+
+    //        }
+
+
+          
+
+    //    }
+
+        
+
+    //}
+}
