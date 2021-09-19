b2d74add12b89b2919f35734f0cc4a80add7a7de
HaziqIrfan21 <90903576+HaziqIrfan21@users.noreply.github.com>
2021-09-20 07:19:15 +0800

    Add files via upload
    - seal Class
    -Static Constructor
    -Class overloading
    -Calculator

---
+++ Age/Age/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Age
+{
+    class Age
+    {
+        public int age;
+        public int ID;
+        public string Name;
+
+        public Age(int i, int x ,string s )
+        {
+            age = i;
+            ID = x;
+            Name = s;
+        }
+
+        public Age (int i)
+        {
+            age = i;
+        }
+        public void print()
+        {
+            //Console.WriteLine("Age: " + age);
+            //Console.WriteLine("ID: " + ID );
+            //Console.WriteLine("Name: " + Name);
+            Console.WriteLine( age);
+            Console.WriteLine( ID);
+            Console.WriteLine(Name);
+        }
+    }
+}
---
+++ Age/Age/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace Age
+{
+    class Program
+    {
+        //char Operator;
+        //float num1, num2;
+        static char Operator;
+        static float num1, num2;
+        static bool loop = true;
+        static int plus = 0;
+        static int minus = 0;
+        static int times = 0;
+        static int divide = 0;
+        static int sum = 0;
+
+        public static void Add()
+        {
+            Console.WriteLine(num1 + num2);
+            plus++;
+        }
+
+        public static void Minus()
+        {
+            Console.WriteLine(num1 - num2);
+            minus++;
+        }
+
+        public static void Times()
+        {
+            Console.WriteLine(num1 * num2);
+            times++;
+        }
+
+        public static void Divide()
+        {
+            Console.WriteLine(num1 / num2);
+            divide++;
+        }
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Details of Tom");
+            Age tom = new Age(10,1998,"Tom");
+            tom.print();
+
+            Console.WriteLine("Age of Richard");
+            Age richard = new Age(12);
+            richard.print();
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            //char Operator;
+            //float num1, num2;
+            //bool loop = true;
+            //int plus = 0;
+            //int minus = 0;
+            //int times = 0;
+            //int divide = 0;
+
+            Console.WriteLine("Calculator");
+            while (loop)
+            {
+                Console.WriteLine("Input first number");
+                num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Input operator");
+                Operator = char.Parse(Console.ReadLine());
+                Console.WriteLine("Input second number");
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+                //Console.WriteLine("Amount of Plus: " + plus);
+                //Console.WriteLine("Amount of Minus: " + minus );
+                //Console.WriteLine("Amount of Times: " + times);
+                //Console.WriteLine("Amount of Divide: " + divide);
+
+
+                switch (Operator)
+                {
+                    case '+':
+                        //Console.WriteLine(num1 + num2);
+                        //plus++;
+                        Add();
+                        break;
+
+                    case '-':
+                        //Console.WriteLine(num1 - num2);
+                        //minus++;
+                        Minus();
+                        break;
+
+                    case '*':
+                        //Console.WriteLine(num1 * num2);
+                        //times++;
+                        Times();
+                        break;
+
+                    case '/':
+                        //Console.WriteLine(num1 / num2);
+                        //divide++;
+                        Divide();
+                        break;
+
+                    case '~':
+                        loop = false;
+                        break;
+
+                    default:
+
+                        break;
+
+                }
+                Console.WriteLine("Amount of Plus: " + plus);
+                Console.WriteLine("Amount of Minus: " + minus);
+                Console.WriteLine("Amount of Times: " + times);
+                Console.WriteLine("Amount of Divide: " + divide);
+                sum = plus + minus + times + divide;
+                Console.WriteLine("Total Amount: " + sum);
+
+            }
+
+         
+
+
+
+        }
+    }
+}
