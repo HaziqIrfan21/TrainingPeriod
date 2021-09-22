7cfea8e497d14254eab11979dbeb8d92c4e2d101
HaziqIrfan21 <90903576+HaziqIrfan21@users.noreply.github.com>
2021-09-22 10:33:36 +0800

    Add files via upload
    HCFLCM
    -Write C# program to find HCF and LCM of two numbers

---
+++ HCFLCM/HCFLCM/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace HCFLCM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter First number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second number");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("LCM of " + a + " and " + b + " is " + LowestCommonMultiple(a, b));
+            Console.WriteLine("HCF of " + a + " and " + b + " is " + GreatestCommonDenominator(a, b));
+
+        }
+
+        static int GreatestCommonDenominator(int a, int b)
+        {
+            if(a == 0)
+            {
+                return b;
+            }
+
+            return GreatestCommonDenominator(b % a, a);
+        }
+
+        static int LowestCommonMultiple(int a, int b)
+        {
+            return (a / GreatestCommonDenominator(a, b) * b);
+        }
+
+
+    }
+
+    
+}
