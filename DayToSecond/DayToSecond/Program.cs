93fde732db523884a10c4b205e95876300957b2b
HaziqIrfan21 <90903576+HaziqIrfan21@users.noreply.github.com>
2021-09-22 10:24:21 +0800

    Add files via upload
    DayToSecond
    -find out the total number of days and sessions you have joined till now then convert the total time into seconds.
    
    Using boxing and unboxing concept

---
+++ DayToSecond/DayToSecond/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DayToSecond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter days:");
+            int days = Convert.ToInt32(Console.ReadLine());
+            int seconds = days * 24 * 60 * 60;
+            Console.WriteLine(seconds + " Seconds");
+
+            //Boxing Unboxing
+
+            int num = 12;
+            object obj = num;
+
+            object obj1 = 15;
+            int num1 = (int)obj;
+        }
+    }
+}
