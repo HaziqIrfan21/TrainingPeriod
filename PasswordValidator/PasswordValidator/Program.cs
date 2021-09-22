93a25a34feff65566284b7ea42fb5969dcaf0c70
HaziqIrfan21 <90903576+HaziqIrfan21@users.noreply.github.com>
2021-09-22 10:21:02 +0800

    Add files via upload
    PasswordValidator
    -validates a password to conform to the following rules:
    
    Length between 6 and 24 characters.
    At least one uppercase letter (A-Z).
    At least one lowercase letter (a-z).
    At least one digit (0-9).
    Maximum of 2 repeated characters.
    "aa" is OK 👍
    "aaa" is NOT OK 👎
    Supported special characters:
    ! @ # $ % ^ & * ( ) + = _ - { } [ ] : ; " ' ? < > , .

---
+++ PasswordValidator/PasswordValidator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace PasswordValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool loop = true;
+            //Console.WriteLine("Hello World!");
+
+            while (loop)
+            {
+                Console.WriteLine("Enter password");
+
+                var input = Console.ReadLine();
+                Console.WriteLine(PasswordChecker(input));
+            }
+           
+            //var hasNumber = new Regex(@"[0-9]+");
+            //var hasUpperChar = new Regex(@"[A-Z]+");
+            //var hasMinimum8CharsMax24Chars = new Regex(@".{6,24}");
+
+
+        }
+
+        public static bool PasswordChecker(string password)
+        {
+            var hasNumber = new Regex(@"[0-9]+");
+            var hasUpperChar = new Regex(@"[A-Z]+");
+            var hasLowerChar = new Regex(@"[a-z]+");
+            var NoRepeats = new Regex(@"[A-Za-z0-9]\{0,2} +");
+            var hasMinimum8CharsMax24Chars = new Regex(@".{6,24}");
+          
+            //var hasNoDuplicate = new Regex(@"[A-Za-z0-9]");
+
+            var isCorrect = hasNumber.IsMatch(password) && hasUpperChar.IsMatch(password) && hasLowerChar.IsMatch(password) && NoRepeats.IsMatch(password) && hasMinimum8CharsMax24Chars.IsMatch(password);
+            if (isCorrect == true)
+            {
+                return true;
+            }
+            else
+                return false;
+            
+        }
+    }
+}
