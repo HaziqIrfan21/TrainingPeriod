559a0b2c0ccd537e155bd17004d5adecb8b94ea4
HaziqIrfan21 <90903576+HaziqIrfan21@users.noreply.github.com>
2021-09-24 10:27:57 +0800

    Add files via upload
    EarthQuake
    -Calculate probability of earthquake and tsunami

---
+++ EarthQuake/EarthQuake/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace EarthQuake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Earthquake
+    {
+        string nameOfPlace;
+        int intensityOfEarthquake;
+
+        void earthquake(string name, int intensity)
+        {
+            nameOfPlace = name;
+            intensityOfEarthquake = intensity;
+        }
+    }
+
+    class Tsunami
+    {
+        void tsunami(float probability)
+        {
+
+        }
+    }
+}
