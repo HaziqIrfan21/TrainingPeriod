076f7d0be64550bdc72929ddc2eb6e49c7c7fd5e
HaziqIrfan21 <90903576+HaziqIrfan21@users.noreply.github.com>
2021-09-27 18:02:59 +0800

    Add files via upload
    Weekly Assignment 3

---
+++ Haziq_Assignment3/Question3.1/Question3.1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Question3._1
+{
+    class Program
+    {
+        static int lottery_No;
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Press 1 to enter the Lottery");
+            //Console.ReadLine();
+
+            
+            LotteryManager lotteryManager = new LotteryManager();
+
+            // Instantiate random number generator
+            Random rand = new Random();
+
+            //Generate Numbers
+            Console.WriteLine("Printing 3 random numbers");
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.WriteLine("{0} -> {1}", i, rand.Next());
+                lottery_No = Convert.ToInt32(Console.ReadLine());
+                lotteryManager[i] = lottery_No.ToString();
+                //Console.WriteLine(lottery_No);
+            }
+            Console.WriteLine(lottery_No);
+
+
+
+
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    lottery_No = lotteryManager.GetRandNumber(); ;
+            //}
+
+
+
+            ////Assign number to customer
+           
+            lotteryManager[1] = lottery_No.ToString();
+            lotteryManager[2] = lottery_No.ToString();
+
+            Console.WriteLine("Lottery number = {0}", lotteryManager[0]);
+            Console.WriteLine("Lottery number = {0}", lotteryManager[1]);
+            Console.WriteLine("Lottery number = {0}", lotteryManager[2]);
+
+
+        }
+    }
+}
---
+++ Haziq_Assignment3/Question3.4/Question3.4/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Question3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int tracking_Id = 0;
+
+            PackageManager packageManager = new PackageManager();
+
+            packageManager.Receive();
+
+            foreach (var package in packageManager.PackageList )
+            {
+                Console.WriteLine("Package Details: {0}, {1}, {2}, {3},", package.id, package.sender_Name, package.receiver_Name, package.receiver_Address);
+            }
+
+            packageManager.Dispatch(tracking_Id);
+
+            foreach (var package in packageManager.PackageList)
+            {
+                Console.WriteLine("Package Details: {0}, {1}, {2}, {3},", package.id, package.sender_Name, package.receiver_Name, package.receiver_Address);
+            }
+        }
+    }
+
+
+    public class Package
+    {
+        public int id;
+        public string sender_Name;
+        public string receiver_Name;
+        public string receiver_Address;
+
+        public Package(int ID, string Sender_Name,string Receiver_Name,string Receiver_Address)
+        {
+            this.id = ID;
+            this.sender_Name = Sender_Name;
+            this.receiver_Name = Receiver_Name;
+            this.receiver_Address = Receiver_Address;
+        }
+
+        int ID
+        {
+            get 
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+        string Sender_Name
+        {
+            get
+            {
+                return sender_Name;
+            }
+
+            set
+            {
+                sender_Name = value;
+            }
+        }
+        string Receiver_Name
+        {
+            get
+            {
+                return receiver_Name;
+            }
+
+            set
+            {
+                receiver_Name = value;
+            }
+        }
+        string Receiver_Address
+        {
+            get
+            {
+                return receiver_Address;
+            }
+
+            set
+            {
+                receiver_Address = value;
+            }
+        }
+
+
+    }
+
+     class PackageManager
+    {
+        public List<Package> PackageList = new List<Package>();
+
+        //Test Package
+        
+       public void Receive()
+        {
+            PackageList.Add(new Package(0, "Sender_name", "receiver_name", "receiver_adress"));
+        }
+
+        public void Dispatch(int tracking_ID)
+        {
+           
+
+            PackageList.RemoveAt(tracking_ID);
+        }
+    }
+}
