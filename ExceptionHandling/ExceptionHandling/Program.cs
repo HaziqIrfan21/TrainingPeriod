4e66ba065d1015af900d2b7e370af431ee3a66d3
HaziqIrfan21 <90903576+HaziqIrfan21@users.noreply.github.com>
2021-09-29 10:08:07 +0800

    Add files via upload
    ExceptionHandling
    - Divide multiply exception handling

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static int a, b, sum;
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Program program = new Program();
+                    Console.WriteLine("Enter first int");
+                    int a = Int32.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter second int");
+                    int b = Int32.Parse(Console.ReadLine());
+                    program.Multiply(a, b);
+                    program.Divide(a, b);
+                  
+                    //Console.WriteLine("result is : " + res);
+                }
+                catch (DivideByZeroException res)
+                {
+
+                    Console.WriteLine("Divided by zero");
+                }
+                catch (NullReferenceException res)
+                {
+                    Console.WriteLine("Null refrence error");
+                }
+
+                catch (FormatException res)
+                {
+                    Console.WriteLine("format exception error");
+                }
+            }
+        }
+
+        public void Multiply(int a, int b)
+        {
+            Console.WriteLine(a * b);
+        }
+
+        public void Divide(int a, int b)
+        {
+             Console.WriteLine(a / b);
+        }
+    }
+}
