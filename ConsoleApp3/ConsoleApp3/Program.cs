eb3c9cbb901b04ccf5c21c48242ff0faf4022dbf
HaziqIrfan21 <90903576+HaziqIrfan21@users.noreply.github.com>
2021-09-30 15:27:38 +0800

    Add files via upload
    ConsoleApp3
    -Generics example

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+
+    class Parent
+    {
+
+    }
+
+    class child : Parent
+    {
+        public string someProperty { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 4;
+            swap<int>(ref a, ref b);
+
+            float c = 1.5f;
+            float d = 5.5f;
+            swap<float>(ref c, ref d);
+
+            string s1 = "Something";
+            string s2 = "Someone";
+            swap<string>(ref s1, ref s2);
+
+            GenericAtClassLevel();
+        }
+
+        public static void swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+
+        
+        
+
+        public class Device<T> // T: child
+        {
+            public T name { get; set; }
+
+            public T catagory { get; set; }
+
+            //public void getProperty()
+            //{
+            //    catagory.someProperty = "something";
+            //}
+
+        }
+
+        public class SampleClass<T>
+        {
+            T[] arrObj = new T[5];
+            int count = 8;
+
+            public void Add(T item)
+            {
+                if (count + 1<5)
+                {
+                    arrObj[count] = item;
+                }
+                count++;
+            }
+
+            public T this[int index]
+            {
+                get { return arrObj[index]; }
+                set { arrObj[index] = value; }
+            }
+        }
+
+        private static void GenericAtFunctionInsideAClass()
+        {
+            SampleClass<float> floatclass = new SampleClass<float>();
+            floatclass.Add(3.14f);
+            floatclass.Add(2.14f);
+            floatclass.Add(4.14f);
+            floatclass.Add(5.14f);
+            floatclass.Add(1.14f);
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(floatclass[i]);
+            }
+
+            SampleClass<int> intclass = new SampleClass<int>();
+            intclass.Add(3);
+            intclass.Add(2);
+            intclass.Add(4);
+            intclass.Add(5);
+            intclass.Add(1);
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(intclass[i]);
+            }
+
+            SampleClass<bool> boolclass = new SampleClass<bool>();
+            boolclass.Add(true);
+            boolclass.Add(false);
+            boolclass.Add(true);
+            boolclass.Add(false);
+            boolclass.Add(true);
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(intclass[i]);
+            }
+        }
+
+        private static void GenericAtClassLevel()
+        {
+
+            //Classname<type> objectname = new class<type>();
+            Device<int> intObj = new Device<int>();
+            Device<float> floatObj = new Device<float>();
+            Device<string> stringObj = new Device<string>();
+
+            intObj.name = 1;
+            intObj.catagory = 2;
+
+            floatObj.name = 5.1f;
+            floatObj.catagory = 3.4f;
+
+            stringObj.name = "somename";
+            stringObj.catagory = "somecatagory";
+
+            Console.WriteLine($"The name is {intObj.name} in {intObj.catagory}");
+            Console.WriteLine($"The name is {floatObj.name} in {floatObj.catagory}");
+            Console.WriteLine($"The name is {stringObj.name} in {stringObj.catagory}");
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/program2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+
+    public interface IMyGenericInterface<T>
+    {
+
+        void Dosomething(T a, T b);
+
+        T DoSomethingElse(T a, T b);
+
+
+    }
+
+    public class MyGenericClass : IMyGenericInterface<int>
+
+    {
+        public void Dosomething(int a, int b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DoSomethingElse(int a, int b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class MyGenericClass1<T> : IMyGenericInterface<T>
+
+    {
+        public void Dosomething(T a, T b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T DoSomethingElse(T a, T b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class program2
+    {
+        
+
+        public void creategenerictypes()
+        {
+            MyGenericClass mygenericclass = new MyGenericClass();
+            mygenericclass.Dosomething(1, 1);
+
+
+            MyGenericClass1<int> mygenericclass1 = new MyGenericClass1<int>();
+            mygenericclass1.Dosomething(1, 1);
+
+            MyGenericClass1<string> mygenericclass2 = new MyGenericClass1<string>();
+            mygenericclass2.Dosomething("hello","goodbye");
+
+        }
+        
+    }
+}
